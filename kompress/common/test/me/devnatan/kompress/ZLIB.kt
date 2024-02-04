11b3934f463ef9535904be9df02916a521586c02
natan.nascimento <natanvnascimento@gmail.com>
2024-02-04 00:03:26 -0300

    Native ZLIB draft impl

--- kompress/common/test/me/devnatan/kompress/ZLIB.kt
+++ kompress/common/test/me/devnatan/kompress/ZLIB.kt
@@ -1,15 +1,23 @@
 package me.devnatan.kompress
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ZLIB {
-
     @Test
-    fun `zlib inflate`() {
+    fun `zlib inflate and deflate`() {
+        val text = "Hello, this is a test string."
+        val compressed = zlibDeflate(text.encodeToByteArray())
 
-    }
+        assertEquals(
+            expected = "[31, -117, 8, 0, 0, 0, 0, 0, 0, 19, -13, 72, -51, -55, -55, -41, 81, 40, -55, -56, 44, 86, 0, -94, 68, -123, -110, -44, -30, 18, -123, -30, -110, -94, -52, -68, 116, 61, 0, 20, -121, -54]",
+            actual = compressed.contentToString(),
+        )
 
-    @Test
-    fun `zlib deflate`() {
+        val decompressed = zlibInflate(compressed)
+        assertEquals(
+            expected = text,
+            actual = decompressed.decodeToString(),
+        )
     }
 }
--- kompress/native/src/me/devnatan/kompress/ZLIB.native.kt
+++ kompress/native/src/me/devnatan/kompress/ZLIB.native.kt
@@ -5,42 +5,70 @@ import kotlinx.cinterop.alloc
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.ptr
 import kotlinx.cinterop.refTo
-import kotlinx.cinterop.value
-import platform.posix.size_tVar
-import platform.zlib.compress
+import kotlinx.cinterop.reinterpret
+import platform.zlib.Z_DEFAULT_COMPRESSION
+import platform.zlib.Z_DEFAULT_STRATEGY
+import platform.zlib.Z_DEFLATED
+import platform.zlib.Z_FINISH
+import platform.zlib.Z_OK
 import platform.zlib.compressBound
-import platform.zlib.uncompress
+import platform.zlib.deflate
+import platform.zlib.deflateEnd
+import platform.zlib.deflateInit2
+import platform.zlib.inflate
+import platform.zlib.inflateEnd
+import platform.zlib.inflateInit2
+import platform.zlib.z_stream
+
+private const val BUF_SIZE = 4096
+
+// Z_OK = 0
+// Z_STREAM_END = 1
+// Z_FINISH = 4
+// Z_DEFLATED = 8
 
 @OptIn(ExperimentalForeignApi::class)
-internal actual fun zlibInflate(input: ByteArray): ByteArray = memScoped {
-    val size = compressBound(input.size.toULong()).toInt()
-    val output = ByteArray(size)
+internal actual fun zlibInflate(input: ByteArray): ByteArray =
+    memScoped {
+        val stream = alloc<z_stream>()
+        stream.avail_in = input.size.toUInt()
+        stream.next_in = input.refTo(0).getPointer(memScope).reinterpret()
 
-    compress(
-        dest = output.asUByteArray().refTo(0),
-        destLen = alloc<size_tVar> {
-            value = size.toULong()
-        }.ptr,
-        source = input.asUByteArray().refTo(0),
-        sourceLen = input.size.toULong(),
-    )
+        inflateInit2(stream.ptr, 15 + 16)
 
-    output.copyOf(size)
-}
+        var buf = ByteArray(BUF_SIZE)
+        var result: Int
+        do {
+            val size = buf.size.toULong()
+            if (stream.total_out >= size) {
+                buf = buf.copyOf(BUF_SIZE)
+            }
+
+            stream.avail_out = (size - stream.total_out).toUInt()
+            stream.next_out = buf.refTo(stream.total_out.toInt()).getPointer(memScope).reinterpret()
+
+            result = inflate(stream.ptr, Z_FINISH)
+        } while (result == Z_OK)
+        inflateEnd(stream.ptr)
+
+        return buf.copyOf(stream.total_out.toInt())
+    }
 
 @OptIn(ExperimentalForeignApi::class)
-internal actual fun zlibDeflate(input: ByteArray): ByteArray = memScoped {
-    val size = input.size * 2
-    val output = ByteArray(size)
+internal actual fun zlibDeflate(input: ByteArray): ByteArray =
+    memScoped {
+        val compressedBuffer = ByteArray(compressBound(input.size.toULong()).toInt())
 
-    uncompress(
-        dest = output.asUByteArray().refTo(0),
-        destLen = alloc<size_tVar> {
-            value = size.toULong()
-        }.ptr,
-        source = input.asUByteArray().refTo(0),
-        sourceLen = input.size.toULong()
-    )
+        val stream = alloc<z_stream>()
+        stream.avail_in = input.size.toUInt()
+        stream.next_in = input.refTo(0).getPointer(memScope).reinterpret()
+        stream.next_out = compressedBuffer.refTo(0).getPointer(memScope).reinterpret()
+        stream.avail_out = compressedBuffer.size.toUInt()
 
-    output.copyOf(size)
-}
+        deflateInit2(stream.ptr, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 15 + 16, 8, Z_DEFAULT_STRATEGY)
+        deflate(stream.ptr, Z_FINISH)
+        deflateEnd(stream.ptr)
+
+        val compressedSize = compressedBuffer.size - stream.avail_out.toInt()
+        return compressedBuffer.copyOf(compressedSize)
+    }

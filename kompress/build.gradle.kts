6ff0401fba149b7c52e7c5bf164cc28599ba4570
natan.nascimento <natanvnascimento@gmail.com>
2024-02-04 00:03:17 -0300

    Install linter

--- kompress/build.gradle.kts
+++ kompress/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.9.20"
+    id("org.jmailen.kotlinter") version "4.2.0"
 }
 
 group = "me.devnatan"
@@ -12,20 +13,13 @@ kotlin {
     val hostOs = System.getProperty("os.name")
     val isArm64 = System.getProperty("os.arch") == "aarch64"
     val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
+    when {
         hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
         hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
         hostOs == "Linux" && isArm64 -> linuxArm64("native")
         hostOs == "Linux" && !isArm64 -> linuxX64("native")
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
-    nativeTarget.apply {
-        compilations.getByName("main") {
-            cinterops {
-            }
-        }
     }
 
     sourceSets.configureEach {
